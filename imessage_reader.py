ea2086ff17f10a43772ad8fa6c2878d2d82991d2
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2019-06-16 15:59:48 +0200

    add fetch_message_data

--- imessage_reader.py
+++ imessage_reader.py
@@ -10,17 +10,27 @@ Date created: 14.06.2019
 """
 
 
-import os
+from os.path import expanduser
 from imessage_reader import common
 
 VERSION = '1.0.1'
 
-# The path to the imessage database
+# The path to the iMessage database
 # DB_PATH = '/Users/{0}/Library/Messages/chat.db'.format(os.getlogin())
-DB_PATH = '/Users/{0}/Desktop/chat.db'.format(os.getlogin())  # for testing only
+DB_PATH = expanduser("~") + '/Desktop/chat.db'  # for testing only
 
 
 # TODO: Move class to separate file.
+class MessageData:
+    def __init__(self, user_id, text):
+        self.user_id = user_id
+        self.text = text
+
+    def __repr__(self):
+        return f"{self.user_id}: {self.text}\n\n"
+
+
+"""
 class Recipient:
     def __init__(self, rowid, phone_id):
         self.rowid = rowid
@@ -37,48 +47,24 @@ class Messages:
 
     def __repr__(self):
         return f"{self.handle_id}: {self.message}"
+"""
 
 
-def fetch_recipients():
-    sql_command = "SELECT ROWID, id from handle"
+def fetch_message_data():
+    sql_command = "SELECT message.text, handle.id FROM message JOIN handle on message.handle_id=handle.ROWID"
     rval = common.fetch_db_data(DB_PATH, sql_command)
 
-    recipients = []
+    data = []
     for row in rval:
-        recipients.append(Recipient(row[0], row[1]))
-
-    return recipients
-
-
-def fetch_messages():
-    sql_command = "SELECT text, handle_id from message"
-    rval = common.fetch_db_data(DB_PATH, sql_command)
-
-    messages = []
-    for row in rval:
-        messages.append(Messages(row[0], row[1]))
-
-    return messages
-
-
-def show_data(r, m):
-    print(m)
-
-
+        data.append(MessageData(row[1], row[0]))
+    return data
 
 def main():
     """
     The entry point of this program.
     """
-    recipients = fetch_recipients()
-    #
-    # first_recipient = recipients[0]
-    # print(first_recipient.rowid)
-    # print(first_recipient.phone_id)
-
-    messages = fetch_messages()
-
-    show_data(recipients, messages)
+    message_data = fetch_message_data()
+    print(message_data)
 
 
 if __name__ == '__main__':
